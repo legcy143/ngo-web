ae3b7664ed0c22df17a40d5f1ce25e0b2bd76180
legcy <legcy143@gmail.com>
2023-05-22 11:23:55 +0530

    fixing text

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -4,15 +4,15 @@ export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div class="px-4 py-6 mx-auto lg:py-8 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div class="relative flex items-center justify-between lg:justify-center lg:space-x-16">
-        <ul class="flex items-center hidden space-x-8 lg:flex">
+    <div className="px-4 py-6 mx-auto lg:py-8 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="relative flex items-center justify-between lg:justify-center lg:space-x-16">
+        <ul className="flex items-center hidden space-x-8 lg:flex">
           <li>
             <a
               href="#donate"
               aria-label="Donate"
               title="Donate"
-              class=" font-semibold bg-zinc-200 px-3 py-1 rounded-lg text-black tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+              className=" font-semibold bg-zinc-200 px-3 py-1 rounded-lg text-black tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
              >
               Donate
             </a>
@@ -22,17 +22,17 @@ export default function Navbar() {
           href="#"
           aria-label="Company"
           title="Company"
-          class="inline-flex items-center"
+          className="inline-flex items-center"
         >
           <img src="/header_logo.png" alt="" className="h-20 lg:h-32" />
         </a>
-        <ul class="flex items-center hidden space-x-8 lg:flex">
+        <ul className="flex items-center hidden space-x-8 lg:flex">
           <li>
             <a
               href="#stats"
               aria-label="Statistics"
               title="Statistics"
-              class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
             >
               Statistics
             </a>
@@ -42,20 +42,20 @@ export default function Navbar() {
               href="#about"
               aria-label="About Us"
               title="About Us"
-              class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
             >
               About Us
             </a>
           </li>
         </ul>
-        <div class="lg:hidden">
+        <div className="lg:hidden">
           <button
             aria-label="Open Menu"
             title="Open Menu"
-            class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
             onClick={() => setIsMenuOpen(true)}
           >
-            <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
               <path
                 fill="currentColor"
                 d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -71,25 +71,25 @@ export default function Navbar() {
             </svg>
           </button>
           {isMenuOpen && (
-            <div class="absolute top-0 left-0 w-full">
-              <div class="p-5 bg-zinc-100 border rounded shadow-sm">
-                <div class="flex items-center justify-between mb-4">
+            <div className="absolute top-0 left-0 w-full">
+              <div className="p-5 bg-zinc-100 border rounded shadow-sm">
+                <div className="flex items-center justify-between mb-4">
                   <div>
                     <a
                       href="#"
                       aria-label="Company"
                       title="Company"
-                      class="inline-flex items-center"
+                      className="inline-flex items-center"
                     ></a>
                   </div>
                   <div>
                     <button
                       aria-label="Close Menu"
                       title="Close Menu"
-                      class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                      className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                       onClick={() => setIsMenuOpen(false)}
                     >
-                      <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                      <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                         <path
                           fill="currentColor"
                           d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -99,13 +99,13 @@ export default function Navbar() {
                   </div>
                 </div>
                 <nav>
-                  <ul class="space-y-4">
+                  <ul className="space-y-4">
                     <li>
                       <a
                         href="#"
                         aria-label="Our product"
                         title="Our product"
-                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                       >
                         Product
                       </a>
@@ -115,7 +115,7 @@ export default function Navbar() {
                         href="#"
                         aria-label="Our product"
                         title="Our product"
-                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                       >
                         Donate
                       </a>
@@ -125,7 +125,7 @@ export default function Navbar() {
                         href="#"
                         aria-label="Product Our Team"
                         title="Product Our Team"
-                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                       >
                         Our Team
                       </a>
@@ -135,7 +135,7 @@ export default function Navbar() {
                         href="#"
                         aria-label="Statistics"
                         title="Statistics"
-                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                       >
                         Statistics
                       </a>
@@ -145,7 +145,7 @@ export default function Navbar() {
                         href="#"
                         aria-label="About Us"
                         title="About Us"
-                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                       >
                         About Us
                       </a>
--- components/home/Hero.js
+++ components/home/Hero.js
@@ -6,8 +6,8 @@ export default function Hero() {
       <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
         <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
-          "Empowering Tomorrow <br/>
-            <span className="text-indigo-700">{" "}Together Today"{" "}</span>
+         {'"'} Empowering Tomorrow {'"'}<br/>
+            <span className="text-indigo-700">Together Today</span>
           </h1>
           <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
           Empowering Communities through Education, Development, and Welfare: Our Mission for a Better Tomorrow          </p>
